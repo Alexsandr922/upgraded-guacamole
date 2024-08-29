fe806419e5695f67fd5590bc0ec5d5f34239e244
Alexsandr922 <komogortsev1342@yandex.ru>
2024-08-29 15:02:08 +0800

    Delete module_2_3..py

--- module_2_3..py
+++
@@ -1,8 +0,0 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-a = 0
-while my_list[0] >= len(my_list):
-    if my_list >= [0]:
-        print(my_list[0])
-    elif my_list <= [0] or my_list == 0:
-        print("отрицательное число")
-    break
